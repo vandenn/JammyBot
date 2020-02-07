c104e92ccc1bcdfe96264aec5b343cbbc087a47d
Vandenn <evan.livelo@gmail.com>
2020-02-07 15:45:48 +0800

    feat: Make thanks detection more flexible

--- logic/messages/salutations/thanks.js
+++ logic/messages/salutations/thanks.js
@@ -2,7 +2,7 @@ const preprocessor = require('../../preprocessor.js');
 
 exports.getThanksMessage = text => {
   text = preprocessor.removeNonAlphanumeric(text);
-  if (!text.includes("thank"))
+  if (!text.match(/tha+nk/i) && !text.match(/t[hn]+x/i))
     return "";
   return "Whatever.";
 }
