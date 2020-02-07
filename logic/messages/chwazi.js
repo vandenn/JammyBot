b8da6ba908c87ed214f52c4299cfefe21fb3c7a5
Vandenn <evan.livelo@gmail.com>
2020-02-07 19:53:25 +0800

    feat: Add response to fight provocations

--- logic/messages/chwazi.js
+++ logic/messages/chwazi.js
@@ -6,5 +6,7 @@ exports.getChwaziMessage = text => {
     return "Chwazi!";
   else if (text.match(/sh[aou]*la/i))
     return "It's pretty shala, man.";
+  else if (text.match(/fight/))
+    return "Let's go right now! Y'all need to be taught a lesson!";
   return "";
 }
