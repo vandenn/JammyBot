1061160c5081d69fac181852220ef571860bcaee
Vandenn <evan.livelo@gmail.com>
2020-02-07 16:48:36 +0800

    feat: Add inside joke shala

--- logic/messages/chwazi.js
+++ logic/messages/chwazi.js
@@ -2,7 +2,9 @@ const preprocessor = require('../preprocessor.js');
 
 exports.getChwaziMessage = text => {
   text = preprocessor.removeNonAlphanumeric(text);
-  if (!text.includes("chwazi"))
-    return "";
-  return "Chwazi!";
+  if (text.includes("chwazi"))
+    return "Chwazi!";
+  else if (text.match(/sh[aou]*la/i))
+    return "It's pretty shala, man.";
+  return "";
 }
