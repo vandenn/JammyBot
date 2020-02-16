959753248b37c474655f64958a028455a5f860d0
Vandenn <evan.livelo@gmail.com>
2020-02-16 14:04:30 +0800

    refactor: Lower priority of math module

--- logic/messages/questions/index.js
+++ logic/messages/questions/index.js
@@ -1,9 +1,9 @@
 const features = require('./features.js');
 const weather = require('./weather.js');
 const news = require('./news.js');
-const math = require('./math.js');
 const recommender = require('./recommender.js');
 const time = require('./time.js');
+const math = require('./math.js');
 const eightball = require('./eightball.js');
 
 exports.getMessages = async text => {
@@ -11,9 +11,9 @@ exports.getMessages = async text => {
   messages.push(features.getFeaturesMessage(text));
   messages.push(await weather.getWeatherMessage(text));
   messages.push(await news.getNewsMessage(text));
-  messages.push(math.getMathMessage(text));
   messages.push(await recommender.getRecommendationMessage(text));
   messages.push(time.getTimeMessage(text));
+  messages.push(math.getMathMessage(text));
   messages = messages.filter(message => message);
   if (messages.length > 0) return messages;
   else return [eightball.getEightBallMessage(text)];
